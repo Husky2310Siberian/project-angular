54fb0f33db1789359c9906ae904af43557c01ea9
Siberian <vasileios.kritikos@aueb.gr>
2025-01-24 10:58:16 +0200

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet , RouterLink} from '@angular/router';
-
+import { ListGroupMenuComponent } from "./components/list-group-menu/list-group-menu.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ListGroupMenuComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
